c5e651121acb3a9773020a34af78494f97343391
Kim Min-Jeong <mjee222@naver.com>
2020-05-14 20:13:15 +0900

    메뉴1 레이아웃 진행중

---
+++ app/src/main/java/com/ogada/menu01Activity_2.java
@@ -0,0 +1,14 @@
+package com.ogada;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class menu01Activity_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu01_2);
+    }
+}
