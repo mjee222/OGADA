aad8d19f46b02fa16bd232bfd02c848ce2d23d65
Kim Min-Jeong <mjee222@naver.com>
2020-05-14 21:20:18 +0900

    all menu done.

---
+++ app/src/main/java/com/ogada/menu02Activity_1.java
@@ -0,0 +1,14 @@
+package com.ogada;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class menu02Activity_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu02_1);
+    }
+}
