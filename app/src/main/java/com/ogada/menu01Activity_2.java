485d863a193d91cf99c6ae3cffaf5e6c629a59ce
Kim Min-Jeong <mjee222@naver.com>
2020-06-04 21:44:34 +0900

    여권 선택 작업중

--- app/src/main/java/com/ogada/menu01Activity_2.java
+++ app/src/main/java/com/ogada/menu01Activity_2.java
@@ -2,13 +2,21 @@ package com.ogada;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 public class menu01Activity_2 extends AppCompatActivity {
@@ -19,11 +27,17 @@ public class menu01Activity_2 extends AppCompatActivity {
     private static String CountryID="";
     String PassportNumber="";
 
+    private Activity activity;
+    private View view;
+    public static menu01_ListAdapter listviewadapter;
+    public static ListView listview;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu01_2);
-
+        activity = this;
         Intent intent=getIntent();
         String CountryNameKor=intent.getExtras().getString("CountryNameKor");
         String CountryNameEng=intent.getExtras().getString("CountryNameEng");
@@ -41,6 +55,7 @@ public class menu01Activity_2 extends AppCompatActivity {
         goBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                showDialog();
                 Intent intent=new Intent(getApplicationContext(), menu01Activity_3.class);
                 intent.putExtra("resID", resID);
                 intent.putExtra("CountryID", CountryID);
@@ -75,6 +90,26 @@ public class menu01Activity_2 extends AppCompatActivity {
         }
 
         return resID;
+    }
+
+    public void showDialog(){
+        // 뷰 호출
+        view = activity.getLayoutInflater().inflate(R.layout.activity_menu01_select_passport, null);
+        // 해당 뷰에 리스트뷰 호출
+        listview = (ListView)view.findViewById(R.id.menu01_select_list01);
+        // 리스트뷰에 어뎁터 설정
+        listview.setAdapter(listviewadapter);
+
+        // 다이얼로그 생성
+        AlertDialog.Builder listViewDialog = new AlertDialog.Builder(activity);
+        // 리스트뷰 설정된 레이아웃
+        listViewDialog.setView(view);
+        // 확인버튼
+        listViewDialog.setPositiveButton("확인", null);
+        // 타이틀
+        listViewDialog.setTitle("ListView DiaLog");
+        // 다이얼로그 보기
+        listViewDialog.show();
     }
 
     @Override
