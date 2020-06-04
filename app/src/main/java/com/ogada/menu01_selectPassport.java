a63d68d79a7d0c7da8b328de0d1105bf5fb0bbaf
Kim Min-Jeong <mjee222@naver.com>
2020-06-04 21:07:02 +0900

    여권 선택 작업중

---
+++ app/src/main/java/com/ogada/menu01_selectPassport.java
@@ -0,0 +1,14 @@
+package com.ogada;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class menu01_selectPassport extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu01_select_passport);
+    }
+}
