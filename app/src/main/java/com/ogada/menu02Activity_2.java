43c6ee20dd952611fd96ad781f83b316398b0862
Kim Min-Jeong <mjee222@naver.com>
2020-06-06 16:47:05 +0900

    카메라 구현 중

--- app/src/main/java/com/ogada/menu02Activity_2.java
+++ app/src/main/java/com/ogada/menu02Activity_2.java
@@ -1,84 +1,47 @@
 package com.ogada;
 
-import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-import android.annotation.TargetApi;
-import android.app.Activity;
-import android.content.ContentResolver;
-import android.content.ContentValues;
-import android.content.Context;
-import android.content.Intent;
+import android.Manifest;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.ImageFormat;
-import android.graphics.Matrix;
-import android.hardware.Camera;
-import android.hardware.Sensor;
-import android.hardware.SensorManager;
-import android.hardware.camera2.CameraAccessException;
-import android.hardware.camera2.CameraCaptureSession;
-import android.hardware.camera2.CameraCharacteristics;
+import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
-import android.hardware.camera2.CameraMetadata;
-import android.hardware.camera2.CaptureFailure;
-import android.hardware.camera2.CaptureRequest;
-import android.hardware.camera2.CaptureResult;
-import android.hardware.camera2.TotalCaptureResult;
-import android.hardware.camera2.params.StreamConfigurationMap;
-import android.media.ExifInterface;
-import android.media.Image;
-import android.media.ImageReader;
-import android.net.Uri;
-import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
-import android.os.Handler;
-import android.os.HandlerThread;
-import android.provider.MediaStore;
-import android.util.DisplayMetrics;
-import android.util.Log;
 import android.util.Size;
-import android.util.SparseIntArray;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
+import android.view.Surface;
+import android.view.TextureView;
 import android.view.View;
 import android.widget.Button;
-import android.widget.FrameLayout;
-import android.widget.ImageView;
 import android.widget.Toast;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.nio.ByteBuffer;
-import java.util.Arrays;
 
-
-public class menu02Activity_2 extends AppCompatActivity{
+public class menu02Activity_2 extends AppCompatActivity implements Camera2APIs.Camera2Interface, TextureView.SurfaceTextureListener {
 
     Button PicBtn;
-    private File file;
-    private CameraSurfaceView surfaceView;
+    private TextureView mTextureView;
+    private Camera2APIs mCamera;
+    private final int MY_PERMISSIONS_REQUEST_CAMERA=1001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu02_2);
 
-        surfaceView=findViewById(R.id.menu02_2_surface);
+        grantCameraPermission();
 
+        mTextureView = (TextureView) findViewById(R.id.menu02_2_cam);
+        mTextureView.setSurfaceTextureListener(this);
+        mCamera = new Camera2APIs(this);
 
-        PicBtn = (Button)findViewById(R.id.menu02_2_picbtn);
+        PicBtn = (Button) findViewById(R.id.menu02_2_picbtn);
         PicBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                capture();
             }
         });
 
@@ -92,55 +55,107 @@ public class menu02Activity_2 extends AppCompatActivity{
         });
     }
 
-    private void capture() {
-        surfaceView.capture(new Camera.PictureCallback() {
-            @Override
-            public void onPictureTaken(byte[] data, Camera camera) {
-                BitmapFactory.Options options = new BitmapFactory.Options();
-                options.inSampleSize = 8;
-                Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
-                //imageView.setImageBitmap(bitmap);
-                // 사진을 찍게 되면 미리보기가 중지된다. 다시 미리보기를 시작하려면...
-                camera.startPreview();
-            }
-        });
+    private void openCamera() {
+        CameraManager cameraManager = mCamera.CameraManager_1(this);
+        String cameraId = mCamera.CameraCharacteristics_2(cameraManager);
+        mCamera.CameraDevice_3(cameraManager, cameraId);
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        // 인텐트에 정보가 포함되어 넘어오게 됨.
+    public void onCameraDeviceOpened(CameraDevice cameraDevice, Size cameraSize) {
+        SurfaceTexture texture = mTextureView.getSurfaceTexture();
+        texture.setDefaultBufferSize(cameraSize.getWidth(), cameraSize.getHeight());
+        Surface surface = new Surface(texture);
 
-        if(requestCode == 101 && resultCode == Activity.RESULT_OK){
-            /* 이미지 파일의 용량이 너무 커서 그대로 앱에 띄울 경우
-             * 메모리 부족으로 비정상 종료될 수 있으므로 크기를 줄여 비트맵으로 로딩한 후 설정 */
+        mCamera.CaptureSession_4(cameraDevice, surface);
+        mCamera.CaptureRequest_5(cameraDevice, surface);
+    }
 
-            BitmapFactory.Options options = new BitmapFactory.Options();
-            options.inSampleSize = 8; // 1/8 로 크기를 줄임
-            Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath(), options);
-            //imageView.setImageBitmap(bitmap);
+    @Override
+    protected void onResume() {
+        super.onResume();
 
-            try {
-                File sdcard = Environment.getExternalStorageDirectory().getAbsoluteFile();
-                String imgpath = sdcard + "/DCIM/image/image_" + Long.toString(System.currentTimeMillis()) + ".jpg";
-                file = new File(imgpath);
-                FileOutputStream fileStream = new FileOutputStream(file);
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 80, fileStream);
-                fileStream.flush();
-                fileStream.close();
-            }catch (FileNotFoundException e) {
-                Log.e("Log", e.toString());
-            } catch (IOException e) {
-                Log.e("Log", e.toString());
+        if (mTextureView.isAvailable()) {
+            openCamera();
+        } else {
+            mTextureView.setSurfaceTextureListener(this);
+        }
+    }
+
+    /* Surface Callbacks */
+    @Override
+    public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int i, int i1) {
+        openCamera();
+    }
+
+    @Override
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surfaceTexture, int i, int i1) {
+
+    }
+
+    @Override
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
+        return true;
+    }
+
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) {
+
+    }
+
+    private void closeCamera() {
+        mCamera.closeCamera();
+    }
+
+    @Override
+    protected void onPause() {
+        closeCamera();
+        super.onPause();
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+    }
+
+    private void grantCameraPermission() {
+        int permssionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+
+        if (permssionCheck != PackageManager.PERMISSION_GRANTED) {
+
+            Toast.makeText(this, "권한 승인이 필요합니다", Toast.LENGTH_LONG).show();
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.CAMERA)) {
+                Toast.makeText(this, "카메라 권한이 필요합니다.", Toast.LENGTH_LONG).show();
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMERA);
+                Toast.makeText(this, "카메라 권한이 필요합니다.", Toast.LENGTH_LONG).show();
+
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_CAMERA: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Toast.makeText(this,"승인이 허가되어 있습니다.",Toast.LENGTH_LONG).show();
+
+                } else {
+                    Toast.makeText(this,"아직 승인받지 않았습니다.",Toast.LENGTH_LONG).show();
+                }
+                return;
             }
 
         }
     }
 
-
-    @Override
-    public void finish(){
-        super.finish();
-        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-    }
 }
